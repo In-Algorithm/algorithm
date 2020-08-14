d3519ac2c9c7d645f7b505b6ee4daf54fa6bf131
borobong2 <69666944+borobong2@users.noreply.github.com>
2020-08-14 15:53:18 +0900

    3-2.cpp(law of large numbers)
    This is Coding Test

---
+++ 3-2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> v;
+
+int main() {
+	// N, M, K를 공백을 기준으로 구분하여 입력받기
+	cin >> n >> m >> k;
+	
+	// N개의 수를 공백을 기준으로 구분하여 입력받기
+	for (int i=0; i < n; i++) {
+		int x;
+		cin >> x;
+		v.push_back(x);
+	} 
+	
+	sort(v.begin(), v.end());	// 입력 받은 수를 정렬하기
+	int first = v[n - 1];	// 가장 큰 수
+	int second = v[n - 2];	// 두 번째로 큰 수
+	
+	//가장 큰 수가 더해지는 횟수 계싼
+	int cnt = (m / (k+1)) * k;
+	result += cnt * first;	// 가장 큰 수 더하기
+	result += (m - cnt) * second;	// 두 번째로 큰 수 더하기
+	
+	cout << result << '\n'; 최종답안 출력 
+}
