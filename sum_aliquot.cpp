89dacaa889fc300c73b93b333e0cc6c208e079fd
borobong2 <69666944+borobong2@users.noreply.github.com>
2021-03-10 14:00:31 +0900

    Create sum_aliquot.cpp

---
+++ sum_aliquot.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n,i, sum = 1;
+  
+  cin >> n;
+  cout << "1";
+  for(i = 2; i<n; i++){
+  	if(n%i==0) {
+  		cout << " + " << i;
+  		sum +=i;
+	}
+  		
+  }
+  cout << " = " << sum;
+  return 0;
+}
+
+/*
+
+#include<iostream>
+using namespace std;
+int main(){
+	//freopen("input.txt", "rt", stdin);
+	int n, sum=1, i;
+	cin>>n;
+	cout<<"1";
+	for(i=2; i<n; i++){
+		if(n%i==0){
+			sum+=i;
+			cout<<" + "<<i;
+		}
+	}
+	cout<<" = "<< sum;
+	return 0;
+}
+*/
