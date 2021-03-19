82fddc9fcf7289124a92fffb32f34e1e87d80ac6
borobong2 <69666944+borobong2@users.noreply.github.com>
2021-03-19 23:34:53 +0900

    Create 30numberOf3Large.cpp

---
+++ 30numberOf3Large.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, lt = 1,rt, cur, k=1 , res;
+	cin >> n;
+	
+	while(lt != 0) {
+			lt=n/(k*10);
+			rt=n%k;
+			cur=(n/k)%10;
+			if(cur>3) res=res+(lt+1)*k;
+			else if(cur == 3) res = res+((lt*k)+(rt+1));
+			else res = res +(lt*k);
+			k*=10;
+	}
+	cout << res;
+	return 0;
+}
