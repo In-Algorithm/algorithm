bdfdea0637b40229ee6751e956ce6fa64c905ab8
borobong2 <69666944+borobong2@users.noreply.github.com>
2021-03-21 21:47:34 +0900

    Create 32sort.cpp

---
+++ 32sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (){
+	
+	int n, tmp, idx;
+	vector<int> a(n);
+	cin >> n;
+	for(int i = 0; i<n; i++){
+		idx = i;
+		for(int j=i+1; j<n; j++){
+			if(a[j] <a[idx]) idx = j;
+		}
+		tmp=a[i];
+		a[i] = a[idx];
+		a[idx] = tmp;
+	}
+	for(int i =0; i<n; i++){
+		cout << a[i];
+	}
+	return 0;
+}
